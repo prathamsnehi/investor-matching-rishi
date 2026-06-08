21fa08049a8038d7a4feb507acdd19b938a218f2
parv <parv@rhour.com>
2026-06-08 22:12:03 +0530

    Added embedding service as internal api

--- backend/src_ml/Dockerfile.ml
+++ backend/src_ml/Dockerfile.ml
@@ -1,10 +1,11 @@
-FROM python:3.13-slim
+FROM python:3.11-slim
 
 RUN pip install --no-cache-dir uv
 
 WORKDIR /src_ml
 
 COPY pyproject.toml ./
+COPY uv.lock ./
 
 ENV UV_PROJECT_ENVIRONMENT=/usr/local
 
@@ -12,4 +13,4 @@ RUN uv sync
 
 COPY . .
 
-EXPOSE 8001
+EXPOSE 8000
---
+++ backend/src_ml/services/vectoriser.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from sentence_transformers import SentenceTransformer
+
+from typing import Dict, List, Any
+
+class Vectoriser:
+    def __init__(self):
+        self.model = SentenceTransformer("all-MiniLM-L6-v2")
+
+    def embed(self, text: str | List[str]) -> List[float]:
+        embeddings = self.model.encode(text).tolist()
+        return embeddings
+    
+class EmbeddingRequest(BaseModel):
+    text: str | List[str]
+
+app = FastAPI(title="Embedding service")
+vectoriser = Vectoriser()
+
+@app.get('healthcheck')
+def health_check() -> Dict[str, str]:
+    return{
+        "status" : "Healthy",
+        "model" : "all-MiniLM-L6-v2"
+    }
+
+@app.post("/embed")
+def embed_text(req: EmbeddingRequest) -> Dict[str. Any]:
+    try:
+        embeddings = vectoriser.embed(req.text)
+        return {
+            "embeddings" : embeddings,
+            "dimensions" : len(embeddings[0]) if isinstance(req.text, list) else len(embeddings),
+            "status": 200,
+            "message": "success",
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=e)
+    
+    
+
+
